3f37b3ed8eecfb1f6fc44a9167c6e10ea7f31960
andrew <n.coop912@gmail.com>
2024-02-15 16:30:21 -0800

    todo: broken compile

--- TurboClone.cpp
+++ TurboClone.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-#include "headers/getPath.hpp"
+#include "src/getPath.cpp"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     // version info
     string version = "0.0.1";
     string art = "Splash coming soon";
 
     // splash
-    cout << version << endl;
-    cout << art << endl;
+    // cout << version << endl;
+    // cout << art << endl;
 
-
+    getPath();
 
 
     return 0;
--- src/getPath.cpp
+++ src/getPath.cpp
@@ -1,6 +1,8 @@
-#include "getPath.hpp"
+#include "../headers/getPath.hpp"
 
 #include <filesystem>
+#include <iostream>
+#include <unistd.h>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -8,7 +10,16 @@ namespace fs = std::filesystem;
 string getPath()
 {
     // if not specified, call with default directory
-    return getPath("archive");
+    cout << "No path specified, defaulting to ~/Documents/archive/" << endl;
+    string user = getlogin();
+    fs::path defaultDir = fs::path("/home") / user / "Documents/archive/";
+    cout << defaultDir << endl;
+
+    string homeDir = getenv("HOME");
+    cout << homeDir << endl;
+    // if ()
+    // return getPath();
+    return "exit";
 }
 
 string getPath(fs::path directory)
